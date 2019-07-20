6b0b6824e6739a37a894c99b7f299821684b7e5c
Piotr Arciszewski <arciszewski92@gmail.com>
2019-07-20 14:56:22 +0200

    chnageS

--- restaurant/assets/js/main.js
+++ restaurant/assets/js/main.js
@@ -1,19 +1,21 @@
 let arrowUp = document.getElementById('arrow-up');
+let navigation = document.getElementsByTagName('nav')[0];
+
+
 
 window.addEventListener('scroll', function () {
     if (window.pageYOffset > 0) {
-        arrowUp.classList.add('show')
+        arrowUp.classList.add('show');
+        navigation.classList.add('white');
     } else {
         arrowUp.classList.remove('show');
+        navigation.classList.remove('white');
     }
 })
 
-arrowUp.addEventListener('click', scrollToTop);
-
-
-function scrollToTop() {
+arrowUp.addEventListener('click', function () {
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
     })
-}
+})
