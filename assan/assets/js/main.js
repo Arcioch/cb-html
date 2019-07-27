f92a793ac2534a2ef010b0b7a8bca1331ec45402
Piotr Arciszewski <arciszewski92@gmail.com>
2019-07-27 16:44:20 +0200

    changes

--- assan/assets/js/main.js
+++ assan/assets/js/main.js
@@ -13,6 +13,22 @@ let slider = tns({
 //inicjacja animation on scroll 
 AOS.init();
 
-//inicjacja dla pralax
-let paralaxImages = document.getElementsByClassName('paralax');
-new simpleParallax(paralaxImages);
+
+//ususniecie kręciołka jak się załaduje strona
+window.addEventListener('load', function () {
+    let loader = document.getElementsByClassName('loader')[0];
+    setTimeout(function () {
+        loader.classList.add('none');
+    }, 100)
+})
+
+
+//przyklejenie navigacji po scrollu
+let navigation = document.getElementsByTagName('nav')[0];
+window.addEventListener('scroll', function () {
+    if (window.pageYOffset > 100) {
+        navigation.classList.add('fixed')
+    } else {
+        navigation.classList.remove('fixed')
+    }
+})
