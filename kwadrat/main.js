878d3088e577c9e88b2bf8b9e3870c81f42ed1f0
Piotr Arciszewski <arciszewski92@gmail.com>
2019-09-28 17:02:29 +0200

    changes

--- kwadrat/main.js
+++ kwadrat/main.js
@@ -1,14 +1,23 @@
 let topPosition = 0;
 let leftPosition = 0;
-
+let moveDistance = 1;
+let level = 1;
 let keys = {
     arrowUp: 38,
     arrowDown: 40,
     arrowLeft: 37,
-    arrowRight: 39
+    arrowRight: 39,
+    space: 32
 }
 
 let smallSquare = document.getElementById('small-square');
+let bigSquare = document.getElementsByClassName('square')[0];
+
+let smallSquareWidth = smallSquare.offsetWidth;
+let bigSquareWidth = bigSquare.offsetWidth;
+
+let speed = 150;
+let interval;
 
 document.addEventListener('keyup', function (event) {
     moveSquare(event.keyCode);
@@ -16,22 +25,154 @@ document.addEventListener('keyup', function (event) {
 
 function moveSquare(keyCode) {
     if (keyCode == keys.arrowUp) {
-        if (topPosition > 0) {
-            topPosition--;
-        }
+        clearInterval(interval);
+        interval = setInterval(moveTop, speed);
     } else if (keyCode == keys.arrowDown) {
-        if (topPosition <= 540) {
-            topPosition++;
-        }
+        clearInterval(interval);
+        interval = setInterval(moveDown, speed);
     } else if (keyCode == keys.arrowLeft) {
-        if (leftPosition > 0) {
-            leftPosition--;
-        }
+        clearInterval(interval);
+        interval = setInterval(moveLeft, speed);
     } else if (keyCode == keys.arrowRight) {
-        if (leftPosition <= 540) {
-            leftPosition++;
+        clearInterval(interval);
+        interval = setInterval(moveRight, speed);
+    } else if (keyCode == keys.space) {
+        clearInterval(interval)
+    }
+}
+
+function moveLeft() {
+    if (leftPosition > 0) {
+
+        if ((leftPosition - moveDistance) > 0) {
+            leftPosition = leftPosition - moveDistance;
+        } else {
+            leftPosition = 0;
+        }
+    } else {
+        if (topPosition == 0) {
+            clearInterval(interval);
+            interval = setInterval(moveDown, speed);
+        } else if (topPosition == bigSquareWidth - smallSquareWidth) {
+            clearInterval(interval);
+            interval = setInterval(moveTop, speed);
+        } else {
+            if (topPosition >= Math.floor(bigSquareWidth / 2)) {
+                clearInterval(interval);
+                interval = setInterval(moveDown, speed);
+            } else {
+                clearInterval(interval);
+                interval = setInterval(moveTop, speed);
+            }
         }
     }
+
+    drawSquare();
+}
+
+function moveRight() {
+
+    if (leftPosition < (bigSquareWidth - smallSquareWidth)) {
+        if ((leftPosition + moveDistance) < (bigSquareWidth - smallSquareWidth)) {
+            leftPosition = leftPosition + moveDistance;
+        } else {
+            leftPosition = bigSquareWidth - smallSquareWidth;
+        }
+
+    }
+    else {
+        if (topPosition == 0) {
+            clearInterval(interval);
+            interval = setInterval(moveDown, speed);
+        } else if (topPosition == bigSquareWidth - smallSquareWidth) {
+            clearInterval(interval);
+            interval = setInterval(moveTop, speed);
+        } else {
+            if (topPosition >= Math.floor(bigSquareWidth / 2)) {
+                clearInterval(interval);
+                interval = setInterval(moveDown, speed);
+            } else {
+                clearInterval(interval);
+                interval = setInterval(moveTop, speed);
+            }
+        }
+    }
+
+
+    drawSquare();
+}
+
+function moveDown() {
+    if (topPosition < (bigSquareWidth - smallSquareWidth)) {
+        if ((topPosition + moveDistance) < (bigSquareWidth - smallSquareWidth)) {
+            topPosition = topPosition + moveDistance;
+        } else {
+            topPosition = bigSquareWidth - smallSquareWidth;
+        }
+    } else {
+        if (leftPosition == 0) {
+            clearInterval(interval);
+            interval = setInterval(moveRight, speed);
+        } else if (leftPosition == bigSquareWidth - smallSquareWidth) {
+            clearInterval(interval);
+            interval = setInterval(moveLeft, speed);
+        } else {
+            if (leftPosition >= Math.floor(bigSquareWidth / 2)) {
+                clearInterval(interval);
+                interval = setInterval(moveRight, speed);
+            } else {
+                clearInterval(interval);
+                interval = setInterval(moveLeft, speed);
+            }
+        }
+    }
+    drawSquare();
+}
+
+function moveTop() {
+    if (topPosition > 0) {
+        if (topPosition - moveDistance > 0) {
+            topPosition = topPosition - moveDistance;
+        } else {
+            topPosition = 0;
+        }
+    } else {
+        if (leftPosition == 0) {
+            clearInterval(interval);
+            interval = setInterval(moveRight, speed);
+        } else if (leftPosition == bigSquareWidth - smallSquareWidth) {
+            clearInterval(interval);
+            interval = setInterval(moveLeft, speed);
+        } else {
+            if (leftPosition >= Math.floor(bigSquareWidth / 2)) {
+                clearInterval(interval);
+                interval = setInterval(moveRight, speed);
+            } else {
+                clearInterval(interval);
+                interval = setInterval(moveLeft, speed);
+            }
+        }
+    }
+    drawSquare();
+}
+
+function drawSquare() {
+    upLevel();
     smallSquare.style.top = topPosition + 'px';
     smallSquare.style.left = leftPosition + 'px';
+}
+
+function upLevel() {
+
+    if ((
+        topPosition ==
+        (Math.floor(bigSquareWidth / 2) -
+            Math.floor(smallSquareWidth) / 2)) && (
+            leftPosition ==
+            (Math.floor(bigSquareWidth / 2) -
+                Math.floor(smallSquareWidth) / 2))) {
+        speed = speed - 50;
+        level++;
+        document.getElementById('level').innerHTML = level;
+    }
 }
